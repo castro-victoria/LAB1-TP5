47c7b88a588fc85af68cb508672663f2b392608b
castro-victoria <mariavictoriacastro15@gmail.com>
2024-04-10 21:05:31 -0300

    creacion

---
+++ trabajoPractico5/src/trabajopractico5/TrabajoPractico5.java
@@ -0,0 +1,17 @@
+
+package trabajopractico5;
+
+/**
+ *
+ * @author Castro Maria Victoria
+ */
+public class TrabajoPractico5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
